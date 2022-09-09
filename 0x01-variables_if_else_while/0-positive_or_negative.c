40dfc5cf84a0d991b0f14a9f6aae3a983c9da2c0
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-09 15:07:59 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - to find positive,negative or zzero int.
+ *
+ *Return: 0
+ **/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n >= 1)
+	{
+		printf("%d is positive number\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is a negative number\n", n);
+	}
+	return (0);
+
+}
