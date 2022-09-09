d72bb15c85dcf6fe2140ad0c6787c4c861919029
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-08 20:21:04 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-        /**                                                         
-	 *task 4: print using puts 
-	 */
+/**
+ * main - main block
+ * Return: 0
+ * task 4: print using puts
+*/
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
