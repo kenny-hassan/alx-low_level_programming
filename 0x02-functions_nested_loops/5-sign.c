d685473a49b6a52aed24c27e0aef82dd914be37d
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-13 19:29:44 -0700

    task 6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,16 +6,16 @@
 */
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
-		_putchar('-');
-			return (-1);
+		_putchar('+');
+			return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	_putchar(1);
-	return (1);
+	_putchar('-');
+	return (-1);
 }
