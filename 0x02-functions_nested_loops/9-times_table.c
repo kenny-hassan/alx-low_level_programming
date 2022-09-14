2d43b15bbbc813a20f90888737e747514293b25d
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-13 20:19:50 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - 9 x n
+ */
+void times_table(void)
+{
+	int i;
+
+	for  (i = 0; i <= 9; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 9; j++)
+		{
+			int n = j * i;
+
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (prod <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(n + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
