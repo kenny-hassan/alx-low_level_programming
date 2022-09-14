24c4651cac21b9cea2e69826d2f1bba3d3cda3b2
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-14 00:37:39 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,45 +7,46 @@ void print_times_table(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n <= 15 && n >= 0)
 	{
-		int j;
-
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			int prod = j * i;
+			int j;
 
-			if (j == 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar('0');
+				int prod = j * i;
+
+				if (j == 0)
+				{
+					_putchar('0');
+				}
+				else if (prod <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + prod);
+				}
+				else if (prod > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (prod / 100));
+					_putchar('0' + ((prod / 10) % 10));
+					_putchar('0' + (prod % 10));
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
+					_putchar('0' + (prod % 10));
+				}
 			}
-			else if (prod <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + prod);
-			}
-			else if (prod > 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + (prod / 100));
-				_putchar('0' + ((prod / 10) % 10));
-				_putchar('0' + (prod % 10));
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + (prod / 10));
-				_putchar('0' + (prod % 10));
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
-}
-
