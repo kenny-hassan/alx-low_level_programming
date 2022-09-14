62360c8e9f98fc8ea8e978e014ef16a4d2200f54
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-13 19:36:26 -0700

    tack 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - main
+ * @n: arguement
+ * Return: 1
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,4 +18,5 @@ void print_alphabet_x10(void);
 int _islower(int k);
 int _isalpha(int a);
 int print_sign(int n);
+int _abs(int n);
 #endif
