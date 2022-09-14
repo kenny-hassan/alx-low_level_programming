598eed8e2d11e80e554a47e1cf41c3dcf9b5b19c
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-13 20:39:11 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * add - adding i+j
+ * @i: arguement 1
+ * @j: arguement 2
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -22,4 +22,5 @@ int _abs(int n);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int i, int j);
 #endif
