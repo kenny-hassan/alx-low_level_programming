4bf6eb362ac881cfca3c069e5abfba16fee8fd75
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-13 21:30:51 -0700

    t

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,12 +24,12 @@ void print_to_98(int n)
 	{
 		for (a = n; a <= 98; a++)
 		{
-			printf("%d",a);
+			printf("%d", a);
 			if (a != 98)
 			{
 				printf(", ");
 			}
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
