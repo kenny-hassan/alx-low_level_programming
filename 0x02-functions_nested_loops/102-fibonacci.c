0a252c61055d1ec555ee0050e6eea655a47c8549
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-14 18:33:10 -0700

    task 12

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -header
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	long int x = 0;
+	long int y = x + 1;
+	long int z = x + y;
+
+	for (i = 0; i < 50; i++)
+	{
+		printf("%ld", z);
+		x = y;
+		y = z;
+		z = x + y;
+		if (i < 50)
+		{
+			printf(", ");
+		}
+	}
+	printf("n");
+	return (0);
+}
