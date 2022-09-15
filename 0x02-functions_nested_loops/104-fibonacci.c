5379633fed9781d556d7b30fff0655528bf9db46
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-14 19:35:42 -0700

    task 15

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 /*
- * main -header file
+ * main - header file
  * Return: 0
  */
 int main(void)
 {
-	int i;
+	int i = 2;
 
-	float x = 0;
+	float x = 1;
 	float y = x + 1;
 	float z = x + y;
 
-	for (i = 0; i < 98; i++)
+	printf("%.0f, ", x);
+	printf("%.0f, ", y);
+	while (i < 98)
 	{
+		i++;
 		printf("%.0f", z);
 		x = y;
 		y = z;
 		z = x + y;
-		if (i < 97)
+		if (i < 98)
 		{
 			printf(", ");
 		}
