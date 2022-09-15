87feeb68f53915afba53e81aabda0f9b9971d9b8
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-14 19:42:06 -0700

    ytask 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,24 +20,21 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar('0');
-				}
-				else if (prod <= 9)
+				} else if (prod <= 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + prod);
-				}
-				else if (prod > 99)
+				} else if (prod > 99)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar('0' + (prod / 100));
 					_putchar('0' + ((prod / 10) % 10));
 					_putchar('0' + (prod % 10));
-				}
-				else
+				} else
 				{
 					_putchar(',');
 					_putchar(' ');
