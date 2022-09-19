df5d6d38f0497b3897421870f8ff83a8ee1db326
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-19 09:08:45 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,18 +2,20 @@
 #include "main.h"
 /**
  * main - standard io
- * Return: 0
+ * @a: pointer of array
+ * @n: num of array
  * print_array - print an array
  */
 
 void print_array(int *a, int n)
 {
 	int i;
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 		if (i != n - 1)
 			printf(", ");
 	}
-	printf('\n');
+	printf("\n");
 }
