e470659b90a809e7941c1d5a0da1e370e1fae9b1
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-19 08:10:12 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,11 @@ void puts2(char *str)
 {
 	int i;
 
-	for  (i = 0;  i < _strlen(s); i++)
+	for  (i = 0;  i < _strlen(str); i++)
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(*(s + i));
+			_putchar(*(str + i));
 		}
 	}
 	_putchar('\n');
