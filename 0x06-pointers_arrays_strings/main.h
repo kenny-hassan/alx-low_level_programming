7d3557bee0500c4115d9e2b1fc44598a384d419a
kenny hassan <atanda.hassan97@gmail.com>
2022-09-22 12:48:26 +0000

    task 5

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,7 +6,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
 int _strlen(char *s);
 char *string_toupper(char *a_z);
 char *cap_string(char *str);
