afa4a5e7d6ed962ab8f55c087644411a14787d74
kenny hassan <atanda.hassan97@gmail.com>
2022-09-22 11:03:22 +0000

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _indxf - return true or false .
+ * @a: string
+ * Return: a
+ */
+int _indxf(char a)
+{
+	int i;
+	
+	char capArr[13] = {'\n', '\t', ' ', '.', ',', ';', ',', '!', '?', '(',')', '{', '}'};
+	for (i = 0; i < 13; i++)
+	{
+		if (capArr[i] == a)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ *  * cap_string - capitalizes string
+ *   * @s: string char
+ *    * Return: capitalized string
+ *     */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (_indxf(s[i]))
+			continue;
+		if (s[i] >= 'a' && s[i] <= 'z' && (_indxf(s[i - 1]) || i == 0))
+		s[i] = s[i] - 32;
+	}
+	return (s);
+}
