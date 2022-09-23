6860477a2404d204147f6c194ff2b0d003a14e5c
kenny-hassan <89618777+kenny-hassan@users.noreply.github.com>
2022-09-23 05:29:12 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,13 +9,13 @@ char *rot13(char *str)
 	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char c_alpha[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	int x, y;
-	char a = str[x];
 
 	for (x = 0; str[x] != '\0'; x++)
 	{
 		for (y = 0; y < 54; y++)
 		{
-			if(((a <= 'z' && a >= 'a') || (a <= 'Z' && a  >= 'A')) && a == alpha[y])
+			if (((str[x] <= 'z' && str[x] >= 'a') ||
+			(str[x] <= 'Z' && str[x] >= 'A')) && str[x] == input[ii])
 			{
 				str[x] = c_alpha[y];
 				break;
