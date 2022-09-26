59c89d780f39d1e33187951f8f4c5199c4c96726
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-26 03:00:21 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memeset -set memory with a const.
+ * @s: array to set
+ * @b: value
+ * @n: byte size
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
