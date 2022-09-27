b9c2fc83ab4698449cf4b4d6b534a96bd687eb43
kenny hassan <atanda.hassan97@gmail.com>
2022-09-27 08:24:52 +0000

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  * @accept: accepted bytes
  * Return: returns value
  */
-unsigned int _strspn(char *s, char *accept)
+unsigned int *_strspn(char *s, char *accept)
 {
 	int i, j;
 	unsigned int result = 0;
