7801d0f0042d0f418dbc1e413298086e28cc6d54
kenny hassan <atanda.hassan97@gmail.com>
2022-09-27 08:22:58 +0000

    task 3

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,12 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+
+#include <stdio.h>
+#include <string.h>
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int *_strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
+
 #endif
