9742b1e68b11fbc050ff739d17aeaa673faab37e
kenny-hassan <atanda.hassan97@gmail.com>
2022-09-28 11:45:18 +0000

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,33 @@
 #include "main.h"
 /**
+ * test - test cases
+ * @i: cases
+ * @n: root arguement
+ * Return: root
+ */
+
+int test(int i, int n)
+{
+	int m;
+
+	if (i * i != n)
+	{
+		if (i > n)
+			return (-1);
+		m = test(i + 1, n);
+	}
+	return (0);
+}
+/**
  * _sqrt_recursion - square root
  * @n: integer arguement
  * Return: square root
  */
 int _sqrt_recursion(int n)
 {
-	int j, i = 0;
+	int i = 0;
 
-	if (i * i != n)
-	{
-		if (i > n)
-			return (-1);
-		i = i + 1;
-		j = _sqrt_recursion(n);
-		return (j + 1);
-	}
-	if (_sqrt_recursion(n) == n && n != 1)
+	if (test(i, n) == n && n != 1)
 		return (-1);
-	return (_sqrt_recursion(n));
+	return (test(i, n));
 }
