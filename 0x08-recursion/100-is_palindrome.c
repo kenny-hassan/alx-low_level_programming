76de00aa5311d7e000ccc66d7c9832aca6445d59
kenny-hassan <atanda.hassan97@gmail.com>
2022-09-28 14:42:34 +0000

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,24 +1,22 @@
 #include "main.h"
-
 /**
- * len - helps do it
+ * len - test 1
  * @s: string
  * Return: string length
  */
 int len(char *s)
 {
 	int i = 0;
+
+	if (*s)
 	{
-		if (*s)
-		{
-			i = i + len(s + 1);
-			return (i += 1);
-		}
-		return (0);
+		i = i + len(s + 1);
+		return (i += 1);
 	}
+	return (0);
 }
 /**
- * test - helps more
+ * test - test 2
  * @i: integer i
  * @s: string
  * Return: int value
@@ -31,17 +29,14 @@ int test(int i, char *s)
 			return (0);
 		else
 			return (test(i + 1, s + 1));
-		return (1);
 	}
+	return (1);
 }
-
-
 /**
  * is_palindrome - palindrome
  * @s: string
  * Return: boolean
  */
-
 int is_palindrome(char *s)
 {
 	int i = 1;
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,31 +5,20 @@
  * @n: int to test
  * Return: 1
  */
-
-
-
-
-int test (int i, int n)
+int test(int i, int n)
 {
 	if (n % i == 0 && n != i)
 		return (0);
 	if (n % i != 0 && i < n)
 		return (test(i + 1, n));
-	
 	return (1);
 }
 
-
 /**
  * is_prime_number - check if it's a prime number
- * @n- arguement
+ * @n: arguement
  * Return: return 1 || 0
  */
-
-
-
-
-
 int is_prime_number(int n)
 {
 	int i = 2;
