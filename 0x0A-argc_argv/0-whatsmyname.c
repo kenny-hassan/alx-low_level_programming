4e5fe7eeb1adea108c38903ec20da7981dc94eab
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-30 07:21:22 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define UNUSED(x) (void)(x)
 /**
  * main - a program that prints its name
+ * @argc: argument
+ * @argv: argument pointer
  * Return: 0
  */
 int main(int argc, char* argv[])
 {
-	printf("%s\n", argc[0]);
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
 	return (0);
 }
