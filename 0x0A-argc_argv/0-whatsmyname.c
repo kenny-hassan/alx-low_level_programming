a91ae5ec75bb84a32da94feb25dbae1b01ca81d3
kehinde hassan <atanda.hassan97@gmail.com>
2022-09-30 06:05:05 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints its name
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argc[0]);
+	return (0);
+}
