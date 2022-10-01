e10cd0ef2e6e86eb6a67c02ba29b5a1cfdc0263c
kenny-hassan <89618777+kenny-hassan@users.noreply.github.com>
2022-10-01 05:39:02 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,12 +1,12 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 #include <ctype.h>
 #define UNUSED(x) (void)(x)
 /**
- * StringCheck - string to find
- * @s: string
- * Return: bool
+ * StringCheck - checks string
+ * @s: string to check
+ * Return: boolean
  */
 int StringCheck(char *s)
 {
@@ -15,33 +15,38 @@ int StringCheck(char *s)
 	for (; s[i] != '\0'; i++)
 	{
 		if (!isdigit(s[i]))
+		{
 			return (0);
+		}
 	}
 	return (1);
 }
 /**
- * main - function
- * @argc: argument
- * @argv: pointer
- * Return: 0
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
  */
 int main(int argc, char  *argv[])
 {
-	int i, sum = 0;
+	int i;
+	int result = 0;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			if (StringCheck(argv[i]))
-				sum += atoi(argv[i]);
+			{
+				result += atoi(argv[i]);
+			}
 			else
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		printf("%d\n", sum);
+		printf("%d\n", result);
 		return (0);
 	}
 	else
@@ -49,4 +54,5 @@ int main(int argc, char  *argv[])
 		printf("%d\n", 0);
 		return (1);
 	}
+
 }
