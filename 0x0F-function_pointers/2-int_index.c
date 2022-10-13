cb3da3856e0fc0ffcd2e93aa7942fea330105865
kenny-hassan <atanda.hassan97@gmail.com>
2022-10-13 02:56:08 +0000

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: array to be considered
+ * @size: size of array
+ * @cmp: pointer to an array
+ * Return: returns -1 if no match
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
