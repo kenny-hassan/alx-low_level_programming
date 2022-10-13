ebde6ca53e6b3ee2be92e7c6bdf12c09d1f6c8dd
kenny-hassan <atanda.hassan97@gmail.com>
2022-10-13 02:57:56 +0000

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stdio.lib>
 #include "function_pointers.h"
 /**
  * int_index - searches for an integer
@@ -14,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]))
+			if (cmp(array[i]) != 0)
 				return (i);
 		}
 	}
