d38b924fb68ecdfe9c248bf8b5874b852c5879ef
kehinde hassan <atanda.hassan97@gmail.com>
2022-10-25 07:27:37 -0700

    new

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,15 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- *  * struct list_s - singly linked list
- *   * @str: string - (malloc'ed string)
- *    * @len: length of the string
- *     * @next: points to the next node
- *      *
- *       * Description: singly linked list node structure
- *        * for Holberton project
- *         */
+/*
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 		char *str;
