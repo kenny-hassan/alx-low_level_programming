8fbf28636f087fe38f530beb8ca90812a75d9ee0
kenny-hassan <atanda.hassan97@gmail.com>
2022-10-26 01:26:13 +0000

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,12 +3,12 @@
 /**
  * free_listint - frees a linked list
  * @head: head of the list
- *
  * Return: void
  */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
+
 	while (head != NULL)
 	{
 		temp = head;
