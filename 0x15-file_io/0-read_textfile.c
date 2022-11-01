fa796d0e52ae6a2bda88ae329a63faff3def8613
kehinde hassan <atanda.hassan97@gmail.com>
2022-10-31 19:08:08 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,25 +7,26 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, printed, wrote;
+	ssize_t fd = 0, chk = 0;
 	char *buff;
 
-	buff = malloc(letters);
-	if (buff == NULL)
+	if (!filename || !letters)
 		return (0);
-	if (filename == NULL)
+
+	fd = open(filename, O_RDONLY);
+	if (fd = -1)
 		return (0);
-	fd = open(filename, O_RDWR);
-	if (fd == -1)
+
+	buff = malloc(sizeof(char) * (letters));
+	if (!buff)
 		return (0);
-	printed = read(fd, buff, letters);
-	if (printed == -1)
+
+	chk = read(fd, buff, letters);
+	chk = write(STDOUT_FILENO, buff, chk);
+	if (chk < 0)
 		return (0);
-	wrote = write(STDOUT_FILENO, buff, printed);
-	if (wrote == -1)
-		return (0);
-	if (close(fd) == -1)
-		return (0);
+
+	close(fd);
 	free(buff);
-	return (printed);
+	return (chk);
 }
